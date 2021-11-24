d1c2bd51866f5a42db86529ae3715a637f14c1f8
Enrico Gaffo <enrico.gaffo@gmail.com>
2021-11-24 19:17:00 +0100

    added bash script to run all the necessary .Rmds

---
+++ pipeline_commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## simulate the data sets
+#cd semi_parametric_sim
+#Rscript -e "rmarkdown::render('datasets_simulation.Rmd')"
+## assess the quality of the simulated data sets
+#Rscript -e "rmarkdown::render('assess_simdata_qual.Rmd')"
+#cd ..
+
+## preprocess the simulated data sets
+cd summarized_benchmark
+Rscript -e "rmarkdown::render('prepare_datasets.Rmd')"
+## compute the benchmark performance
+Rscript -e "rmarkdown::render('benchmark.Rmd')"
+## collect and plot the performance evaluations
+Rscript -e "rmarkdown::render('evaluations.Rmd')"
