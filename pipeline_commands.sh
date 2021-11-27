4c721bde51d21022e5cf1166d75ce1a894e6a2c0
Enrico Gaffo <enrico.gaffo@gmail.com>
2021-11-27 11:44:20 +0100

    added running of the script to compute quality assessment of the preprocessed simulated data; uncommented the dataset simulation steps

--- pipeline_commands.sh
+++ pipeline_commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-## simulate the data sets
-#cd semi_parametric_sim
-#Rscript -e "rmarkdown::render('datasets_simulation.Rmd')"
-## assess the quality of the simulated data sets
-#Rscript -e "rmarkdown::render('assess_simdata_qual.Rmd')"
-#cd ..
+# simulate the data sets
+cd semi_parametric_sim
+Rscript -e "rmarkdown::render('datasets_simulation.Rmd')"
+# assess the quality of the simulated data sets
+Rscript -e "rmarkdown::render('assess_simdata_qual.Rmd')"
+cd ..
 
 ## preprocess the simulated data sets
 cd summarized_benchmark
@@ -14,3 +14,5 @@ Rscript -e "rmarkdown::render('prepare_datasets.Rmd')"
 Rscript -e "rmarkdown::render('benchmark.Rmd')"
 ## collect and plot the performance evaluations
 Rscript -e "rmarkdown::render('evaluations.Rmd')"
+## compute quality statistics on the simulated data sets after the pre-processing
+Rscript -e "rmarkdown::render('preprocessed_dataset_sim_qual.Rmd')"
